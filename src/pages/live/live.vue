99ea598d8479e467d6daa2773c2c3280211b50f1
杨俊 <756585379@qq.com>
2017-09-13 20:03:09 +0800

    Enenn

--- src/pages/live/live.vue
+++ src/pages/live/live.vue
@@ -1,11 +1,19 @@
 <template>
   <div>
-    live
+    <img src="" alt="">
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+  import axios from 'axios';
   export default {
+    created() {
+      axios.get('http://saakie.s1.natapp.cc/img_code').then(function (response) {
+        console.log(response);
+      }).catch(function (err) {
+        console.log(err);
+      });
+    }
   };
 </script>
 
